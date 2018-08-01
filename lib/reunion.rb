0e5f2e268e6691694fe0b70a27b513d706a77dd2
SKG360 <samuel.giles@colorado.edu>
2018-08-01 10:40:30 -0600

    Adds test and runner files for iteration three

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,11 @@
 class Reunion
+  def initialize(location)
+    @location = location
+    @activities = []
+  end
+
+  def add_activity(name, base_cost, participant_cost)
+    @name = name 
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,4 +4,15 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+  def test_if_it_exists
+    rt = Reunion.new("Cleveland")
+
+    assert_instance_of Reunion, rt
+  end
+
+  def test_if_it_adds_activity
+    rt = Reunion.new("Cleveland")
+    assert_equal "Surfing", rt.add_activity("Surfing", 100, 50)
+  end
+
 end
